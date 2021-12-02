9a9f4dc52add579aac9ccddf3d692f719345ca66
BellaZZZZZ <bellazhao_321@qq.com>
2021-12-02 21:27:50 +0800

    计数排序，好像速度并没有更快

--- 506.相对名次.js
+++ 506.相对名次.js
@@ -12,12 +12,18 @@
 var findRelativeRanks = function(score) {
   // 排序可以用sort
   const medal = ['Gold Medal', 'Silver Medal', 'Bronze Medal']
-  const map = new Map(), res = new Array(score.length)
-  score.forEach((value, index) => map.set(value, index))
-  score = score.sort((a, b) => b - a)
-  score.forEach((value, index) => {
-    res[map.get(value)] = index < 3 ? medal[index] : String(index+1)
-  })
+  let max = Math.max(...score)
+  const arr = new Array(max + 1).fill(-1)
+  const res = new Array(score.length)
+  score.forEach((value, index) => arr[value] = index)
+  let rank = 1
+  while(max >= 0) {
+    if(arr[max] >= 0) {
+      res[arr[max]] = rank < 4 ? medal[rank-1] : String(rank)
+      rank++
+    }
+    max--
+  }
   return res
 };
 // @lc code=end
