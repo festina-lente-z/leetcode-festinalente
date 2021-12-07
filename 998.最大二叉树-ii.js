0f2e1ceac6f7df550a9e1ff9f5ff44b95346f90a
BellaZZZZZ <bellazhao_321@qq.com>
2021-12-08 00:09:07 +0800

    空

---
+++ 998.最大二叉树-ii.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=998 lang=javascript
+ *
+ * [998] 最大二叉树 II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoMaxTree = function(root, val) {
+  // 这题和BST插入有点像
+  
+
+};
+// @lc code=end
+
