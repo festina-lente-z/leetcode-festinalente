5014d666dea0ad88b809b656f70234c2a37819de
BellaZZZZZ <bellazhao_321@qq.com>
2021-12-09 21:09:03 +0800

    2-寻找左侧边界的二分查找，性能有所提升

--- 34.在排序数组中查找元素的第一个和最后一个位置.js
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -11,29 +11,21 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-  let left = 0, right = nums.length - 1
+  let left = 0, right = nums.length
 
-  while(left <= right) {
-    let mid = left + Math.floor((right - left) / 2)
-    if(nums[mid] == target) {
-      // 因为题目是要一个区间，在传统二分查找的基础上，分别向左和向右查找
-      let countLeft = -1
-      let countRight = -1
-      // 从mid开始，防止边界溢出
-      for(let i=mid; i>=0; i--){
-        if(nums[i] == target) countLeft++
-        else break
-      }
-      for(let i=mid; i<nums.length; i++){
-        if(nums[i] == target) countRight++
-        else break
-      }
-      return [mid-countLeft, mid+countRight]
-    }
+  while(left<right) {
+    let mid = left + Math.floor((right-left)/2)
+    if(nums[mid] > target) right = mid
+    else if(nums[mid] == target) right = mid
     else if(nums[mid] < target) left = mid + 1
-    else if(nums[mid] > target) right = mid - 1
   }
-  return [-1,-1]
+  if(nums[left] != target) return [-1,-1]
+  let count = -1
+  for(let i=left; i<nums.length; i++){
+    if(nums[i] == target) count++
+    else break
+  }
+  return [left, left+count]
 };
 // @lc code=end
 
