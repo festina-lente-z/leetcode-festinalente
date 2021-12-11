3cfcffbc50d485faaf85de36077323236a2db440
BellaZZZZZ <bellazhao_321@qq.com>
2021-12-12 01:12:28 +0800

    1-AC-在21题的基础上遍历链表数组

--- 23.合并k个升序链表.js
+++ 23.合并k个升序链表.js
@@ -15,9 +15,33 @@
 /**
  * @param {ListNode[]} lists
  * @return {ListNode}
- */
+*/
+// 在21题的基础上
 var mergeKLists = function(lists) {
-
+  let k = lists.length
+  if(k==0) return null
+  if(k==1) return lists[0]
+  let two = lists[0]
+  for(let i=1; i<k; i++){
+    two = mergeTwo(two,lists[i])
+  }
+  return two
 };
+const mergeTwo = (l1, l2) => {
+  let dummy = new ListNode(), p = dummy
+  while(l1 && l2) {
+    if(l1.val < l2.val) {
+      p.next = l1
+      l1 = l1.next
+    } else {
+      p.next = l2
+      l2 = l2.next
+    }
+    p = p.next
+  }
+  if(l1) p.next = l1
+  if(l2) p.next = l2
+  return dummy.next
+}
 // @lc code=end
 
