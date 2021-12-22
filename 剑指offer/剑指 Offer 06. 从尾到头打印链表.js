d2a27c0de7987e25c283950599765bac5bb8cfe2
BellaZZZZZ <bellazhao_321@qq.com>
2021-12-22 22:53:42 +0800

    1-1.非递归思路:头部插入；2.递归思路：结束条件-head==null，自我调用的部分

---
+++ 剑指offer/剑指 Offer 06. 从尾到头打印链表.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var reversePrint = function(head) {
+  if(head == null) return []
+  else return [...reversePrint(head.next), head.val]
+};
